5614c571eeb99b50705ac492dc34dc31822f3d68
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2024-04-19 14:34:56 +0700

    Fix PulseAudio compile error

--- src/engine/audio_io_pulseaudio.cpp
+++ src/engine/audio_io_pulseaudio.cpp
@@ -2,56 +2,66 @@
 
 #ifdef __linux__
 
-namespace wb {
-AudioIO* create_audio_io_pulseaudio() {
-    return nullptr;
-}
-} // namespace wb
-
-#else
-
 extern "C" {
-#include <pulse/simple.h>
 #include <pulse/error.h>
+#include <pulse/simple.h>
 }
 
 namespace wb {
 
 struct AudioIOPulseAudio : public AudioIO {
-
-    pa_simple *pa_connection = nullptr;
-
-    bool rescan_devices() override {
-        return false;
+    pa_simple* pa_connection = nullptr;
+    
+    ~AudioIOPulseAudio() {
+        // do nothing
     }
+
+    bool init() override { return false; }
+
+    bool rescan_devices() override { return false; }
+    
     const AudioDeviceProperties& get_input_device_properties(uint32_t idx) const override {
         return default_input_device;
-    };
+    }
+    
     const AudioDeviceProperties& get_output_device_properties(uint32_t idx) const override {
         return default_output_device;
-    };
+    }
+    
     bool open_device(AudioDeviceID output_device_id, AudioDeviceID input_device_idx) override {
 
         pa_connection = nullptr;
 
         return false;
-    };
+    }
+    
     void close_device() override {
         // do nothing
-    };
+    }
+    
     bool start(bool exclusive_mode, AudioDevicePeriod period, AudioFormat input_format,
-                       AudioFormat output_format, AudioDeviceSampleRate sample_rate) override {
+               AudioFormat output_format, AudioDeviceSampleRate sample_rate) override {
         return false;
-    };
+    }
+
     void end() override {
         // do nothing
-    };
-
-    ~AudioIOPulseAudio() {
-        // do nothing
-    };
+    }
 };
 
+AudioIO* create_audio_io_pulseaudio() {
+    AudioIOPulseAudio* audio_io = new (std::nothrow) AudioIOPulseAudio();
+    if (!audio_io)
+        return nullptr;
+    if (!audio_io->init())
+        return nullptr;
+    return audio_io;
+}
+} // namespace wb
+
+#else
+
+namespace wb {
 AudioIO* create_audio_io_pulseaudio() {
     return nullptr;
 }
