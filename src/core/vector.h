62a4c059e79ebf105efe983208a40def73247689
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2024-05-25 18:53:47 +0700

    Add Vector::emplace_at method

--- src/core/vector.h
+++ src/core/vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "common.h"
+#include "memaccess.h"
 #include "types.h"
 #include <memory>
 
@@ -49,7 +50,9 @@ struct Vector {
         return *this;
     }
 
-    inline Vector<T>& operator=(Vector<T>&& other) noexcept {
+    inline Vector<T>& operator=(Vector<T>&& other) noexcept
+        requires std::movable<T>
+    {
         destroy_();
         intern_ = std::exchange(other.intern_, {});
         return *this;
@@ -92,10 +95,85 @@ struct Vector {
         return intern_.data[idx];
     }
 
-    inline T* begin() { return intern_.data; }
-    inline const T* begin() const { return intern_.data; }
-    inline T* end() { return intern_.data + intern_.size; }
-    inline const T* end() const { return intern_.data + intern_.size; }
+    inline T* begin() noexcept { return intern_.data; }
+    inline const T* begin() const noexcept { return intern_.data; }
+    inline T* end() noexcept { return intern_.data + intern_.size; }
+    inline const T* end() const noexcept { return intern_.data + intern_.size; }
+
+    template <typename... Args>
+    inline T& emplace_at(size_t idx, Args&&... args) {
+        if (intern_.size == intern_.capacity) {
+            size_t new_capacity = grow_capacity_(intern_.capacity + 1);
+            T* new_data = (T*)std::malloc(new_capacity * sizeof(T));
+            assert(new_data && "Failed to allocate new storage");
+            if (intern_.data) {
+                if constexpr (std::is_trivially_copy_constructible_v<T>) {
+                    std::memcpy(new_data, intern_.data, idx * sizeof(T));
+                    std::memcpy(new_data + idx + 1, intern_.data + idx,
+                                (intern_.size - idx) * sizeof(T));
+                } else {
+                    if constexpr (std::move_constructible<T>) {
+                        relocate_by_move(intern_.data, intern_.data + idx, new_data);
+                        relocate_by_move(intern_.data + idx, intern_.data + intern_.size,
+                                         new_data + idx + 1);
+                    } else if constexpr (std::copy_constructible<T>) {
+                        relocate_by_move(intern_.data, intern_.data + idx, new_data);
+                        relocate_by_move(intern_.data + idx, intern_.data + intern_.size,
+                                         new_data + idx + 1);
+                    }
+                }
+                std::free(intern_.data);
+            }
+
+            T* item = nullptr;
+            if constexpr (std::is_aggregate_v<T>) {
+                item = new (new_data + idx) T {std::forward<Args>(args)...};
+            } else {
+                item = new (new_data + idx) T(std::forward<Args>(args)...);
+            }
+
+            intern_.data = new_data;
+            intern_.capacity = new_capacity;
+            intern_.size++;
+            return *item;
+        }
+
+        T* begin_ptr = intern_.data + intern_.size - 1;
+        T* dst_ptr = begin_ptr + 1;
+        T* end_ptr = intern_.data + idx;
+        while (begin_ptr >= end_ptr) {
+            if constexpr (std::movable<T>) {
+                *dst_ptr-- = std::move(*begin_ptr);
+            } else if constexpr (std::copyable<T>) {
+                *dst_ptr-- = *begin_ptr;
+            }
+            begin_ptr--;
+        }
+
+        if (!std::is_trivially_destructible_v<T>) {
+            (intern_.data + idx)->~T();
+        }
+
+        T* item;
+        if constexpr (std::is_aggregate_v<T>) {
+            item = new (intern_.data + idx) T {std::forward<Args>(args)...};
+        } else {
+            item = new (intern_.data + idx) T(std::forward<Args>(args)...);
+        }
+
+        intern_.size++;
+        return *item;
+    }
+
+    template <typename... Args>
+    inline T& emplace_back(Args&&... args) {
+        if (intern_.size == intern_.capacity) {
+            reserve(grow_capacity_(intern_.capacity + 1));
+        }
+        T* new_item = new (intern_.data + intern_.size) T(std::forward<Args>(args)...);
+        intern_.size++;
+        return *new_item;
+    }
 
     inline void push_back(const T& item)
         requires std::copy_constructible<T>
@@ -120,18 +198,41 @@ struct Vector {
     inline void pop_back() noexcept {
         assert(intern_.size > 0);
         intern_.size--;
+        if constexpr (!std::is_trivially_destructible_v<T>)
+            intern_.data[intern_.size].~T();
     }
 
-    inline void resize(size_t new_size) {
+    inline void resize(size_t new_size)
+        requires std::default_initializable<T>
+    {
+        if (new_size > intern_.capacity) {
+            reserve(grow_capacity_(new_size));
+            uninitialized_default_construct(intern_.data + intern_.size, intern_.data + new_size);
+        }
+        intern_.size = new_size;
+    }
+
+    inline void resize_fast(size_t new_size)
+        requires std::is_trivial_v<T>
+    {
+        if (new_size > intern_.capacity) {
+            reserve(grow_capacity_(new_size));
+        }
+        intern_.size = new_size;
+    }
+
+    inline void resize(size_t new_size, T&& default_value)
+        requires std::move_constructible<T>
+    {
         if (new_size > intern_.capacity) {
             reserve(grow_capacity_(new_size));
             if constexpr (!std::is_trivially_default_constructible_v<T>) {
                 T* data_ptr = intern_.data + intern_.size;
                 T* end_ptr = intern_.data + new_size;
                 while (data_ptr != end_ptr)
-                    new (data_ptr++) T();
+                    new (data_ptr++) T(std::move(default_value));
             } else {
-                std::memset(intern_.data, 0, (new_size - intern_.size) * sizeof(T));
+                std::memset(intern_.data + intern_.size, 0, (new_size - intern_.size) * sizeof(T));
             }
         }
         intern_.size = new_size;
@@ -142,10 +243,12 @@ struct Vector {
             return;
         }
         T* new_data = (T*)std::malloc(new_capacity * sizeof(T));
-        assert(new_data);
+        assert(new_data && "Failed to allocate new storage");
         if (intern_.data) {
             if constexpr (std::is_trivially_copy_constructible_v<T>) {
-                std::memcpy(new_data, intern_.data, intern_.size * sizeof(T));
+                if (intern_.size != 0) {
+                    std::memcpy(new_data, intern_.data, intern_.size * sizeof(T));
+                }
             } else {
                 T* new_data_ptr = new_data;
                 T* old_data_ptr = intern_.data;
