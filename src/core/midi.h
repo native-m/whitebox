4d7fe1af18d208974f7000cc62cd49eaddf902d1
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2024-06-29 21:48:41 +0700

    Move midi event types to event.h

--- src/core/midi.h
+++ src/core/midi.h
@@ -1,53 +1,25 @@
 #pragma once
 
 #include "common.h"
+#include "vector.h"
+#include <array>
+#include <filesystem>
 
 namespace wb {
-
-enum class MidiStatus {
-    NoteOn,
-    NoteOff,
-    PolyPressure,
-    ControlChange
-};
-
-struct MidiNoteOnEvent {
-    uint16_t channel;
-    uint16_t note_number;
-    float tuning;
-    float velocity;
-    int32_t length;
-    int32_t note_id;
-};
-
-struct MidiNoteOffEvent {
+struct MidiNote {
+    double min_time;
+    double max_time;
+    uint32_t id;
     uint16_t channel;
     uint16_t note_number;
     float velocity;
-    int32_t note_id;
-    float tuning;
 };
 
-struct MidiPolyPressureEvent {
-    uint16_t channel;
-    uint16_t note_number;
-    float pressure;
-    int32_t note_id;
+struct MidiNoteState {
+    bool on;
+    uint64_t last_tick;
+    float velocity;
 };
 
-struct MidiControlChangeEvent {
-    uint16_t index;
-    uint32_t data;
-};
-
-struct MidiMessage {
-    MidiStatus status;
-    union {
-        MidiNoteOnEvent note_on;
-        MidiNoteOffEvent note_off;
-        MidiPolyPressureEvent poly_pressure;
-        MidiControlChangeEvent control_change;
-    };
-};
-
+Vector<MidiNote> load_notes_from_smf0(const std::filesystem::path& path);
 } // namespace wb
--- src/engine/event.h
+++ src/engine/event.h
@@ -14,13 +14,59 @@ enum class EventType {
     PlayMidi,
 };
 
+enum class MidiMessageType {
+    NoteOn,
+    NoteOff,
+    PolyPressure,
+    ControlChange
+};
+
+struct MidiNoteOnEvent {
+    uint16_t channel;
+    uint16_t note_number;
+    float tuning;
+    float velocity;
+    int32_t length;
+    int32_t note_id;
+};
+
+struct MidiNoteOffEvent {
+    uint16_t channel;
+    uint16_t note_number;
+    float velocity;
+    int32_t note_id;
+    float tuning;
+};
+
+struct MidiPolyPressureEvent {
+    uint16_t channel;
+    uint16_t note_number;
+    float pressure;
+    int32_t note_id;
+};
+
+struct MidiControlChangeEvent {
+    uint16_t index;
+    uint32_t data;
+};
+
+struct MidiMessageEvent {
+    MidiMessageType type;
+    union {
+        MidiNoteOnEvent note_on;
+        MidiNoteOffEvent note_off;
+        MidiPolyPressureEvent poly_pressure;
+        MidiControlChangeEvent control_change;
+    };
+};
+
 struct AudioEvent {
     size_t sample_offset;
     Sample* sample;
 };
 
 struct MidiEvent {
-    
+    double time_offset;
 };
 
 struct Event {
