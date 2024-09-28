ef0b191b9c0eb4d8a388ae9d0e42aeeb27f7cc7b
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2024-09-28 07:28:23 +0700

    [Misc] Improve array container concepts

--- src/core/types.h
+++ src/core/types.h
@@ -15,10 +15,10 @@ concept IsPointerType = std::is_pointer_v<T>;
 
 template <typename T>
 concept DynamicArrayContainer = requires(T v) {
-    { v.at(size_t()) } -> IsReferenceType<>;
-    { v[size_t()] } -> IsReferenceType<>;
-    { v.data() } -> IsPointerType<>;
-    v.size();
+    { v.at(size_t()) } -> IsReferenceType;
+    { v[size_t()] } -> IsReferenceType;
+    { v.data() } -> IsPointerType;
+    { v.size() } -> std::convertible_to<size_t>;
     v.resize(size_t());
     v.reserve(size_t());
 };
@@ -26,7 +26,7 @@ concept DynamicArrayContainer = requires(T v) {
 template <typename T>
 concept ContinuousArrayContainer = requires(T v) {
     { v.data() } -> IsPointerType<>;
-    v.size();
+    { v.size() } -> std::convertible_to<size_t>;
 };
 
 }
