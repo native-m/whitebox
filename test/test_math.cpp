9386850468c57b27d14ac409d23af1c6bb030a9a
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2024-11-02 19:49:15 +0700

    [Test] Create test for maths

---
+++ test/test_math.cpp
@@ -0,0 +1,9 @@
+#include "catch_amalgamated.hpp"
+#include "core/core_math.h"
+
+TEST_CASE("NonLinearDbRange") {
+    wb::NonLinearRange db_range(-72.0f, 6.0f, -2.4f);
+    INFO("Plain to normalized: " << db_range.plain_to_normalized(0.0f));
+    INFO("Normalized to plain: " << db_range.normalized_to_plain(0.0f));
+    //CHECK(false);
+}
