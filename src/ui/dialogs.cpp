d133b1db8ab6ee41e62fffed29fa9d7b5616fe8a
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2025-02-19 17:41:30 +0700

    [UI] Improve confirmation popup dialog

--- src/ui/dialogs.cpp
+++ src/ui/dialogs.cpp
@@ -1,11 +1,17 @@
 #include "dialogs.h"
-#include "engine/engine.h"
 #include <imgui.h>
 
 namespace wb {
+
 bool popup_confirm(const char* str, const char* msg) {
     bool ret = false;
-    if (ImGui::BeginPopupModal(str, nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+    ImGuiWindowClass window_class;
+    window_class.ViewportFlagsOverrideSet |= ImGuiViewportFlags_TopMost; // This popup should be the top-most window
+    ImGui::SetNextWindowClass(&window_class);
+    ImGui::SetNextWindowPos(ImGui::GetWindowViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+    ImGuiWindowFlags popup_flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings;
+    if (ImGui::BeginPopupModal(str, nullptr, popup_flags)) {
         ImGui::TextUnformatted(msg);
         ImGui::Separator();
         ret = ImGui::Button("Ok", ImVec2(120.0f, 0.0f));
@@ -18,4 +24,5 @@ bool popup_confirm(const char* str, const char* msg) {
     }
     return ret;
 }
+
 } // namespace wb
