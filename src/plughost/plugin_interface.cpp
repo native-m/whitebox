15d7bbea47d822fcece36e8bccba67729e4553b4
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2025-07-07 01:09:22 +0700

    [Misc] Fix link error

--- src/plughost/plugin_interface.cpp
+++ src/plughost/plugin_interface.cpp
@@ -3,6 +3,17 @@
 #include "extern/xxhash.h"
 
 namespace wb {
+
 PluginInterface::PluginInterface(uint64_t module_hash, PluginFormat format) : module_hash(module_hash), format(format) {
 }
+
+PluginResult PluginInterface::render_ui() {
+  if (!ImGui::Begin("")) {
+    ImGui::End();
+    return PluginResult::Ok;
+  }
+
+  return PluginResult::Ok;
+}
+
 }  // namespace wb
