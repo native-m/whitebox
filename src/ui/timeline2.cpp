5a67905f3bd9126a705271f41ab2e9cf30ca5ff2
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2025-10-18 23:36:57 +0700

    [UI] Refactor

--- src/ui/timeline2.cpp
+++ src/ui/timeline2.cpp
@@ -1237,10 +1237,10 @@ void timeline_draw_track_lanes(const ImVec2& display_size, const ImVec2& view_si
     const ImVec2 clip_content_max(x1_clipped, clip.pos_y + height);
 
     const Color color(clip.color);
-    const Color label_color = color.darken(0.15f);
-    const Color content_color = color.brighten(1.4f);  // color.darken(1.7f);
-    const ColorU32 bg_color = color.change_alpha(color.a * 0.80f).premult_alpha().to_uint32();
-    const ColorU32 label_color_u32 = label_color.to_uint32();
+    const Color bg_color = color.darken(0.15f);
+    const Color content_color = color.brighten(1.4f);    // color.darken(1.7f);
+    const ColorU32 bg_color_u32 = bg_color.to_uint32();  // color.change_alpha(color.a * 0.80f).premult_alpha().to_uint32();
+    const ColorU32 label_color_u32 = color.to_uint32();
     const ColorU32 content_color_u32 = content_color.to_uint32();
 
     const bool mini_clip = height <= 30.0f;
@@ -1251,9 +1251,9 @@ void timeline_draw_track_lanes(const ImVec2& display_size, const ImVec2& view_si
       dl->AddRect(clip_label_min_bb, clip_content_max, 0x3F000000, 3.0f, ImDrawFlags_RoundCornersTop, 4.5f);
     }
 
-    dl->AddRectFilled(clip_label_min_bb, clip_label_max_bb, color.to_uint32(), 3.0f, ImDrawFlags_RoundCornersTop);
-    dl->AddRectFilled(clip_content_min, clip_content_max, label_color.to_uint32(), 0.5f, ImDrawFlags_RoundCornersBottom);
-    im_draw_hline(dl, clip_label_max_bb.y - 0.5f, clip_content_min.x, clip_content_max.x, bg_color);
+    dl->AddRectFilled(clip_label_min_bb, clip_label_max_bb, label_color_u32, 3.0f, ImDrawFlags_RoundCornersTop);
+    dl->AddRectFilled(clip_content_min, clip_content_max, bg_color_u32, 0.5f, ImDrawFlags_RoundCornersBottom);
+    im_draw_hline(dl, clip_label_max_bb.y - 0.5f, clip_content_min.x, clip_content_max.x, bg_color_u32);
 
     switch (clip.type) {
       case ClipType::Audio: {
